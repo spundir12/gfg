6974650a44e13445447f3e065820db781b0586cd
saransh <saransh@192.168.0.102>
2019-04-08 10:58:12 +0530

    commit

---
+++ DataStructure/src/mustdocoding/array/EquilibriumIndex.java
@@ -0,0 +1,27 @@
+/*
+https://www.geeksforgeeks.org/equilibrium-index-of-an-array/
+ 
+Equilibrium index of an array is an index such that the sum of elements at lower indexes is equal to the sum of elements at higher indexes.
+For example, in an array A:
+
+Example :
+
+Input : A[] = {-7, 1, 5, 2, -4, 3, 0}
+Output : 3
+3 is an equilibrium index, because:
+A[0] + A[1] + A[2]  =  A[4] + A[5] + A[6]
+
+Write a function int equilibrium(int[] arr, int n); that given a sequence arr[] of size n, 
+returns an equilibrium index (if any) or -1 if no equilibrium indexes exist.
+
+*/
+package mustdocoding.array;
+
+public class EquilibriumIndex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- OCP Java 2/src/ocp/concurrency/ReentrantLockExample.java
+++ OCP Java 2/src/ocp/concurrency/ReentrantLockExample.java
@@ -1,7 +1,12 @@
 package ocp.concurrency;
+/*
+  ReentrantLock are provided in java to provide synchronization with more flexibility unlike traditional synchronized blocks
+  It provides more 
+ */
 
 public class ReentrantLockExample {
 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
